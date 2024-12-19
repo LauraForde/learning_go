2c39b81f6ccd8cf12267c251782fa8541dee5a68
LauraForde <lauraforde@outlook.com>
2024-12-19 15:02:25 +0800

    - Amended print lines
    - Amended print lines in the call of the array
    - More aligned with what was shown in the book

--- Page113.go
+++ Page113.go
@@ -11,13 +11,13 @@ func main() {
 	greetings := []string{"Hello", "Hola", "नमस्कार", "こんにちは", "Привіт"}
 
 	// Subslice containing first two values
-	var x []string = greetings[0:2]
+	var x []string = greetings[:2]
 
 	// Subslice containing second, third & fourth values
 	var y []string = greetings[1:4]
 
 	// Subslice containing fourth & fifth values
-	var z []string = greetings[3:5]
+	var z []string = greetings[3:]
 
 	// Printing all slices
 	fmt.Println(greetings)
